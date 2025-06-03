13913029163bc2e8ff720186c679ac328e27975c
abok-cymk <allanabok18@gmail.com>
2025-06-03 15:11:44 +0300

    add base url

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/rest-countries-api-with-color-theme-switcher-master/", 
 });
